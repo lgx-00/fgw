5399796243c3abbf1138642c2d4641bad6bb9307
光贤 刘 <lightspare_liu@qq.com>
2023-11-02 10:51:07 +0800

    refactor(global): 重构全局异常捕获的部分日志输出代码
    1. 更加细致地输出请求参数无法识别时的异常信息，方便调试。
    2. 在输出内容中添加【全局异常处理】前缀。

--- src/main/java/com/pxxy/advice/GlobalExceptionHandler.java
+++ src/main/java/com/pxxy/advice/GlobalExceptionHandler.java
@@ -1,8 +1,13 @@
 package com.pxxy.advice;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.exc.InputCoercionException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.pxxy.exceptions.BaseRuntimeException;
 import com.pxxy.exceptions.ReportException;
 import com.pxxy.utils.ResultResponse;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -19,6 +24,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -38,7 +44,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = ConstraintViolationException.class)
     public ResultResponse<?> constraintViolationException(ConstraintViolationException e) {
-        log.warn("ConstraintViolationException:", e);
+        log.warn("【全局异常处理】ConstraintViolationException:", e);
         Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
         String message = violations.stream().map(ConstraintViolation::getMessage).collect(Collectors.joining("/"));
         return ResultResponse.fail(message);
@@ -47,7 +53,50 @@ public class GlobalExceptionHandler {
     @ResponseBody
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResultResponse<?> error(HttpMessageNotReadableException e) {
-        log.warn("请求参数无法识别", e);
+        Throwable ex = e.getCause();
+        Class<? extends Throwable> exClass = Objects.isNull(ex) ? null : ex.getClass();
+
+        if (InvalidFormatException.class.equals(exClass)) {
+            InvalidFormatException ife = (InvalidFormatException) ex;
+            Class<?> targetType = ife.getTargetType();
+            if (Integer.class.equals(targetType)) {
+                JsonMappingException.Reference ref = ife.getPath().get(ife.getPath().size() - 1);
+                try {
+                    Class<?> model = ref.getFrom().getClass();
+                    String fieldName = ref.getFieldName();
+
+                    ApiModelProperty prop = model.getDeclaredField(fieldName).getAnnotation(ApiModelProperty.class);
+                    String propName = prop == null ? fieldName : prop.value();
+                    log.warn("【全局异常处理】无法通过分析给定的字符串得到 32 位整型数值。给定的字符串为 \"{}\"，" +
+                            "数据绑定模型为 {}，字段名称为 {}, 模型字段名称为 {}。", ife.getValue(),
+                            model, fieldName, propName, e);
+                    return ResultResponse.fail(String.format("“%s”的内容不合法，请输入正确的整数。", propName));
+                } catch (NoSuchFieldException ignored) {}
+                return ResultResponse.fail("请输入正确的整数。");
+            }
+            log.warn("【全局异常处理】无法分析字符串 \"{}\" 得到 \"{}\" 类型的值。", ife.getValue(), targetType.getName(), e);
+        }
+
+        if (JsonMappingException.class.equals(exClass)) {
+            JsonMappingException jme = (JsonMappingException) ex;
+            exClass = jme.getCause().getClass();
+
+            JsonMappingException.Reference ref = jme.getPath().get(jme.getPath().size() - 1);
+            try {
+                ApiModelProperty prop = ref.getFrom().getClass().getDeclaredField(ref.getFieldName())
+                        .getAnnotation(ApiModelProperty.class);
+                String msg = String.format("【全局异常处理】“%s”的内容不合法。", prop.value());
+                log.warn(msg, e);
+                return ResultResponse.fail(msg);
+            } catch (NoSuchFieldException ignored) {}
+        }
+
+        if (Objects.isNull(exClass) || InputCoercionException.class.equals(exClass)
+                || JsonParseException.class.equals(exClass)) {
+            log.warn("【全局异常处理】请求参数无法识别。", e);
+        } else {
+            log.warn("【全局异常处理】未知异常。", e);
+        }
         return ResultResponse.fail("请求参数无法识别");
     }
 
@@ -55,7 +104,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResultResponse<?> error(MissingServletRequestParameterException e) {
-        log.warn("缺少必要参数", e);
+        log.warn("【全局异常处理】缺少必要参数。", e);
         return ResultResponse.fail("缺少必要参数");
     }
 
@@ -92,7 +141,7 @@ public class GlobalExceptionHandler {
         }
 
         if (bindingResult == null) {
-            log.warn("参数校验失败，错误信息：{}", ex.getMessage());
+            log.warn("【全局异常处理】参数校验失败，错误信息：{}", ex.getMessage());
             return ResultResponse.fail(ex.getMessage());
         }
 
@@ -100,13 +149,13 @@ public class GlobalExceptionHandler {
         Object target = bindingResult.getTarget();
         String className = target != null ? target.getClass().getSimpleName() : null;
         if (errors.isEmpty()) {
-            log.warn("参数校验失败，对象名称：{}，对象类型：{}",
+            log.warn("【全局异常处理】参数校验失败，对象名称：{}，对象类型：{}",
                     bindingResult.getObjectName(), className);
             return ResultResponse.fail("部分参数不合法");
         }
         ObjectError objectError = errors.get(0);
         String messages = objectError.getDefaultMessage();
-        log.warn("参数校验失败，对象名称：{}，对象类型：{}，错误信息：{}",
+        log.warn("【全局异常处理】参数校验失败，对象名称：{}，对象类型：{}，错误信息：{}",
                 bindingResult.getObjectName(), className, messages);
         if (messages != null && messages.length() > 50) {
             return ResultResponse.fail("部分参数不合法");
