dca34d9714aadde5363a44680aff5ef99eb8e4a1
光贤 刘 <lightspare_liu@qq.com>
2023-11-02 10:42:49 +0800

    chore: 修改配置及自动部署脚本的服务器 IP 地址
    由于IP变动，修改配置及脚本中的服务器 IP 为最新的 192.168.110.241。

--- autodeploy.sh
+++ autodeploy.sh
@@ -2,9 +2,9 @@
 
 mvn clean package -Dmaven.test.skip=true
 
-scp target/fgw-backend-0.0.1-SNAPSHOT.jar root@192.168.110.239:/mysoft/fgw/archives
+scp target/*.jar root@192.168.110.241:/mysoft/fgw/archives
 
-ssh root@192.168.110.239 /mysoft/fgw/rename.sh
-ssh root@192.168.110.239 'cd /mysoft; /mysoft/restart.sh fgw'
+ssh root@192.168.110.241 /mysoft/fgw/rename.sh
+ssh root@192.168.110.241 'cd /mysoft; /mysoft/restart.sh fgw'
 
 exit 0
