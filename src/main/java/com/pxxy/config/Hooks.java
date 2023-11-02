1d033e7dcf4480488c198a4222b3feb102139cef
光贤 刘 <lightspare_liu@qq.com>
2023-11-02 10:55:28 +0800

    refactor(hook): 重构启动与关闭钩子的运行模式
    将启动与关闭钩子由原先的使用 Bean 实体构建钩子与销毁钩子实现的方式改为使用 Spring 提供的 CommandLineRunner 和 DisposableBean 接口实现的方式，使运行模式更加规范。

--- src/main/java/com/pxxy/config/Hooks.java
+++ src/main/java/com/pxxy/config/Hooks.java
@@ -2,10 +2,9 @@ package com.pxxy.config;
 
 import com.pxxy.utils.TokenUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 /**
  * Class name: Hooks
@@ -17,17 +16,17 @@ import javax.annotation.PreDestroy;
  */
 @Slf4j
 @Component
-public class Hooks {
+public class Hooks implements CommandLineRunner, DisposableBean {
 
-    @PostConstruct
-    public void postConstruct() {
+    @Override
+    public void run(String... args) throws Exception {
         log.info("启动完成，开始载入令牌信息。");
         TokenUtil.load();
         log.info("加载完成。");
     }
 
-    @PreDestroy
-    public void preDestroy() {
+    @Override
+    public void destroy() {
         log.info("接收到关闭信号，开始清理缓存。");
         TokenUtil.cleanup();
         log.info("清理完成。");
